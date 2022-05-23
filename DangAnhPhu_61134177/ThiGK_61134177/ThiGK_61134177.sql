4343096a48f9eb03f9a6965fc07de8f94c372210
danganhphu <phudanganh107@gmail.com>
2022-05-23 08:40:35 +0700

    update

---
+++ DangAnhPhu_61134177/ThiGK_61134177/ThiGK_61134177.sql
@@ -0,0 +1,101 @@
+﻿CREATE DATABASE ThiGK_61134177
+GO
+
+USE ThiGK_61134177
+GO
+
+CREATE TABLE LoaiDocGia
+(
+	MaLoaiDocGia VARCHAR(10) NOT NULL,
+	TenLoaiDocGia NVARCHAR(50) NOT NULL
+)
+GO
+
+
+CREATE TABLE DocGia
+(
+	MaDG VARCHAR(10) NOT NULL,
+	HoDG NVARCHAR(50) NOT NULL,
+	TenDG NVARCHAR(50) NOT NULL,
+	NgaySinh DATE,
+	GioiTinh BIT,
+	Email VARCHAR(50),
+	AnhDG NVARCHAR(50),
+	MaLoaiDocGia VARCHAR(10) NOT NULL
+)
+GO
+
+---- Primary Key
+ALTER TABLE dbo.LoaiDocGia ADD CONSTRAINT PK_LoaiDG PRIMARY KEY(MaLoaiDocGia)
+GO
+
+ALTER TABLE dbo.DocGia ADD CONSTRAINT PK_DocGia PRIMARY KEY(MaDG)
+GO
+
+---Foreign key
+
+ALTER TABLE dbo.DocGia ADD CONSTRAINT FK_DocGia_LoaiDG FOREIGN KEY(MaLoaiDocGia) REFERENCES dbo.LoaiDocGia(MaLoaiDocGia)
+GO
+
+INSERT INTO dbo.LoaiDocGia
+(
+    MaLoaiDocGia,
+    TenLoaiDocGia
+)
+VALUES
+(   'LDG001',
+    N'Sinh Viên'
+    ),
+(   'LDG002',
+    N'Giảng Viên'
+    ),
+(   'LDG003', 
+    N'Phụ Huynh'
+    )
+GO
+
+INSERT INTO dbo.DocGia
+(
+    MaDG,
+    HoDG,
+    TenDG,
+    NgaySinh,
+    GioiTinh,
+    Email,
+    AnhDG,
+    MaLoaiDocGia
+)
+VALUES
+(   'DG01',
+    N'Đặng Anh',
+    N'Phú',
+    CAST(N'2001-10-10' AS Date),
+    1,
+    'phuda@gmail.com',
+    N'docgia1.png',
+    'LDG001'
+    ),
+(   'DG02',
+    N'Đặng Văn',
+    N'Tình',
+    CAST(N'1970-09-20' AS Date),
+    1,
+    'tinhda@gmail.com',
+    N'docgia2.png',
+    'LDG003'
+    ),
+(   'DG03', 
+    N'Nguyễn Thị',
+    N'Hồng',
+    CAST(N'1989-07-12' AS Date),
+    0,
+    'hongnt@gmail.com',
+    N'docgia3.png',
+    'LDG002'
+    )
+GO
+
+SELECT * FROM dbo.DocGia
+GO
+SELECT * FROM dbo.LoaiDocGia
+GO
---
+++ DangAnhPhu_61134177/ThiGK_61134177/ThiGK_61134177/Controllers/DocGia_61134177Controller.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ThiGK_61134177.Models;
+
+namespace ThiGK_61134177.Controllers
+{
+    public class DocGia_61134177Controller : Controller
+    {
+        private ThiGK_61134177Entities db = new ThiGK_61134177Entities();
+
+        // GET: DocGia_61134177
+        public ActionResult Index()
+        {
+            var docGia = db.DocGia.Include(d => d.LoaiDocGia);
+            return View(docGia.ToList());
+        }
+
+        // GET: DocGia_61134177/Details/5
+        public ActionResult GioiThieu_61134177(string id)
+        {
+            return View();
+        }
+
+        // GET: DocGia_61134177/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaLoaiDocGia = new SelectList(db.LoaiDocGia, "MaLoaiDocGia", "TenLoaiDocGia");
+            return View();
+        }
+
+        // POST: DocGia_61134177/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaDG,HoDG,TenDG,NgaySinh,GioiTinh,Email,AnhDG,MaLoaiDocGia")] DocGia docGia)
+        {
+            var imgDG = Request.Files["AnhDG"];
+            string postedFileName = System.IO.Path.GetFileName(imgDG.FileName);
+            var path = Server.MapPath("/images/" + postedFileName);
+            imgDG.SaveAs(path);
+            if (ModelState.IsValid)
+            {
+                docGia.AnhDG = postedFileName;
+                db.DocGia.Add(docGia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLoaiDocGia = new SelectList(db.LoaiDocGia, "MaLoaiDocGia", "TenLoaiDocGia", docGia.MaLoaiDocGia);
+            return View(docGia);
+        }
+
+        // GET: DocGia_61134177/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DocGia docGia = db.DocGia.Find(id);
+            if (docGia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(docGia);
+        }
+
+        // POST: DocGia_61134177/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            DocGia docGia = db.DocGia.Find(id);
+            db.DocGia.Remove(docGia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult TimKiemDG_61134177(string hoTen = "")
+        {
+            ViewBag.hoTen = hoTen;
+            var docGias = from dg in db.DocGia
+                          where (dg.HoDG + " " + dg.TenDG).Contains(hoTen)
+                          select dg;
+            if (docGias.Count() == 0)
+                ViewBag.TB = "Không có thông tin tìm kiếm.";
+
+            return View(docGias.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
